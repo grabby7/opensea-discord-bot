69dcc96cd2c0ff29d3c5409a299827910041f256
grabby7 <cheetith@gmail.com>
2021-08-26 18:42:40 -0500

    Update checkSales.ts

--- checkSales.ts
+++ checkSales.ts
@@ -21,9 +21,6 @@ const  discordSetup = async (): Promise<TextChannel> => {
 }
 
 const buildMessage = (sale: any) => (
-	//if ( (sale.asset.name.substr(0, 8) == "Unigrids") || (sale.asset.name.substr(0, 9) == "Beatboxes") ) {
-	//if ( (sale.asset.name.substr(0, 4) == "Pigm") || (sale.asset.name.substr(0, 4) == "Chro") ) {
-	
 	  new Discord.MessageEmbed()
 		.setColor('#0099ff')
 		.setTitle(sale.asset.name + ' sold!')
@@ -31,7 +28,6 @@ const buildMessage = (sale: any) => (
 		//.setAuthor('OpenSea Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://github.com/sbauch/opensea-discord-bot')
 		.setThumbnail(sale.asset.image_url)
 		.addFields(
-			{ name: 'testGOOD', value: sale.asset.name.substr(0, 4) },
 			{ name: 'Name', value: sale.asset.name },
 			{ name: 'Amount', value: `${ethers.utils.formatEther(sale.total_price || '0')}${ethers.constants.EtherSymbol}`},
 			{ name: 'Buyer', value: sale?.winner_account?.address, },
