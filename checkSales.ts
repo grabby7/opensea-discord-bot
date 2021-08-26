be95f4451e12f86cd06f77566be5179c790d736a
grabby7 <cheetith@gmail.com>
2021-08-26 16:48:46 -0500

    Update checkSales.ts

--- checkSales.ts
+++ checkSales.ts
@@ -22,7 +22,6 @@ const  discordSetup = async (): Promise<TextChannel> => {
 
 const buildMessage = (sale: any) => (
 	//if ( (sale.asset.name.substr(0, 8) == "Unigrids") || (sale.asset.name.substr(0, 9) == "Beatboxes") ) {
-	if ( (sale.asset.name.substr(0, 4) == "Pigm") || (sale.asset.name.substr(0, 4) == "Chro") ) {
 	
 	  new Discord.MessageEmbed()
 		.setColor('#0099ff')
@@ -40,29 +39,6 @@ const buildMessage = (sale: any) => (
 	  //.setImage(sale.asset.image_url)
 		.setTimestamp(Date.parse(`${sale?.created_date}Z`))
 		.setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
-	) else {
-		
-	  new Discord.MessageEmbed()
-		.setColor('#0099ff')
-		.setTitle(sale.asset.name + ' sold! DIFFERENT')
-		.setURL(sale.asset.permalink)
-		//.setAuthor('OpenSea Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://github.com/sbauch/opensea-discord-bot')
-		.setThumbnail(sale.asset.image_url)
-		.addFields(
-			{ name: 'test', value: sale.asset.name.substr(0, 4) },
-			{ name: 'Name', value: sale.asset.name },
-			{ name: 'Amount', value: `${ethers.utils.formatEther(sale.total_price || '0')}${ethers.constants.EtherSymbol}`},
-			{ name: 'Buyer', value: sale?.winner_account?.address, },
-			{ name: 'Seller', value: sale?.seller?.address,  },
-		)
-	  //.setImage(sale.asset.image_url)
-		.setTimestamp(Date.parse(`${sale?.created_date}Z`))
-		.setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
-		
-	}
-	
-	
-	
 	
 )
 
@@ -88,7 +64,9 @@ async function main() {
     
   return await Promise.all(
     openSeaResponse?.asset_events?.reverse().map(async (sale: any) => {
+      if ( (sale.asset.name.substr(0, 4) == "Pigm") || (sale.asset.name.substr(0, 4) == "Chro") ) {
       const message = buildMessage(sale);
+      }
       return channel.send(message)
     })
   );   
